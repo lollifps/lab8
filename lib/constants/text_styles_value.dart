4faa44c97e05f2f52ac4fe143a96624af7a292da
lollifps <mirzarashidov@gmail.com>
2026-03-13 18:15:20 +0500

    Added README

---
+++ lib/constants/text_styles_value.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class AppTextStyles {
+  static const TextStyle title = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primaryColor,
+  );
+
+  static const TextStyle subtitle = TextStyle(
+    fontSize: 18,
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('ru')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('en'),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lab 8',
+      locale: context.locale,
+      supportedLocales: context.supportedLocales,
+      localizationsDelegates: context.localizationDelegates,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("hello".tr()),
+      ),
+      body: Center(
+        child: Text(
+          "welcome".tr(),
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
